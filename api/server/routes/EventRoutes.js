8a49ea2ef389aae11e22c2baedbf7fc2246c66f7
Kevin Van Slyke <kvs713@gmail.com>
2019-12-03 14:18:16 -0600

    Update EventRoutes.js

--- api/server/routes/EventRoutes.js
+++ api/server/routes/EventRoutes.js
@@ -5,8 +5,5 @@ const router = Router();
 
 router.get("/events", EventController.getEvents);
 router.post("/events", EventController.createEvent);
-// router.get("/:id", BookController.getABook);
-// router.put("/:id", BookController.updatedBook);
-// router.delete("/:id", BookController.deleteBook);
 
 export default router;
